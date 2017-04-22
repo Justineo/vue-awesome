d5f4ac10556fc99c0ec4b4c173f3f98cbe2cff45
Justineo <justice360@gmail.com>
2017-04-23 02:09:16 +0800

    fix demo

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../demo/index.html'),
+    assetsRoot: path.resolve(__dirname, '../demo'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '.',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
