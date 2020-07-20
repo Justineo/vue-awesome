01759b04b1a089cc11da3a552d480662cb11d5e9
Justineo <justice360@gmail.com>
2020-07-20 10:35:11 +0800

    chore: add missing files and rebuild demo

---
+++ src/icons/brands/unsplash.js
@@ -0,0 +1,13 @@
+import Icon from '../../components/Icon.vue'
+
+Icon.register({
+  'brands/unsplash': {
+    width: 448,
+    height: 512,
+    paths: [
+      {
+        d: 'M448 230.2v249.8h-448v-249.8h141.1v124.9h165.7v-124.9h141.1zM306.9 32v124.9h-165.7v-124.9h165.7z'
+      }
+    ]
+  }
+})
